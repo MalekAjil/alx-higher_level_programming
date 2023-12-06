8c704f0c98ca32768d12e387468dd274e19088e6
MAlekAjil <aboanasajil@gmail.com>
2023-12-06 11:03:37 -0800

    Add Readme

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    m = matrix.copy()
-    if m is None:
-        return None
-    for i in range(len(m)):
-        for j in range(len(m[i])):
-            m[i][j] *= m[i][j]
-    return m
+    squares = list(map(lambda x: x**2, range(len(matrix))))
+    return squares
