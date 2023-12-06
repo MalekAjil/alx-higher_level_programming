d1349bee4347a1a771e734366db1e6bd9268343f
MAlekAjil <aboanasajil@gmail.com>
2023-12-06 12:01:57 -0800

    add replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squares = list(map(lambda x: x**2, range(len(matrix))))
+    squares = matrix.copy()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            squares[i][j] = matrix[i][j] ** 2
     return squares
