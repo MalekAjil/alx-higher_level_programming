de26a765e2838740ece84da6444ece3867ad186b
MAlekAjil <aboanasajil@gmail.com>
2023-12-06 10:57:24 -0800

    Add Readme

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
+    m = matrix.copy()
+    if m is None:
+        return None
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            m[i][j] *= m[i][j]
+    return m
