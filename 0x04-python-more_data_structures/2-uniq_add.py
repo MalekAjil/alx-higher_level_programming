5322767c7a26d2586d2cd190fa0fb6ac420b9cba
MAlekAjil <aboanasajil@gmail.com>
2023-12-06 18:49:07 -0800

    uniqe add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    sumx = 0
     new_list = []
     for x in my_list:
         if x not in new_list:
             new_list.append(x)
-    return new_list
+            sumx += x
+    return sumx
