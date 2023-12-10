2efb367fa331fa1a440468b8a3fbb283bb31aa04
MAlekAjil <aboanasajil@gmail.com>
2023-12-10 10:56:23 -0800

    cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,3 +1,19 @@
 #include "lists.h"
 
+/**
+ * check_cycle - check the list if it is cyclic or not
+ * @list: the header of the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tmp = list;
 
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		if (tmp == list)
+			return (1);
+	}
+	return (0);
+}
