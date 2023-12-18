d54dcc086f219f040bb7ee229d7cc13659bc9179
MalekAjil <aboanasajil@gmail.com>
2023-12-18 12:19:40 -0800

    try / except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        n = 0;
+        n = 0
         for i in range(x):
             print(my_list[i], end="")
             n += 1
         print()
-    except:
+    except (IndexError, ValueError):
         print()
     return (n)
