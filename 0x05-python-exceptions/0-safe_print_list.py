0b28f769dc7396637ff8f1d283e72701a07a2183
MalekAjil <aboanasajil@gmail.com>
2023-12-18 12:14:54 -0800

    try / except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        n = 0;
+        for i in range(x):
+            print(my_list[i], end="")
+            n += 1
+        print()
+    except:
+        print()
+    return (n)
