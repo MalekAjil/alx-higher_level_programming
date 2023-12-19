f95b9a11c45dda0bf621b8e107a7144cf1e4fbdb
MalekAjil <aboanasajil@gmail.com>
2023-12-19 13:12:16 -0800

    oop

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    ''' represents a square'''
     def __init__(self, size=0):
+        ''' the initializer'''
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class Square:
+    ''' represents a square'''
     def __init__(self, size=0):
+        ''' the initializer'''
         if(type(size) != int):
             raise TypeError("size must be an integer")
         if(size < 0):
