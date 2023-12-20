c4b2fd00515457cb22cbef759d6f5e7180d84018
MalekAjil <aboanasajil@gmail.com>
2023-12-19 18:45:56 -0800

    oop

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """The initializer
+        Args:
+            size (:obj:`int`, optional): the size of the square
+        Attributes:
+            __size (int): the square size"""
+        if(type(size) != int):
+            raise TypeError("size must be an integer")
+        if(size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns Area"""
+        return (self.__size * self.__size)
