1de04cd27249fbf5d7cc73229265d6662150de67
MalekAjil <aboanasajil@gmail.com>
2023-12-26 12:27:50 -0800

    task 100

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def remove_char_at(str, n):
+    if n >= len(str) or n < 0:
+        return str
+    str2 = list(str)
+    str2[n] = ''
+    str3 = ''.join(str2)
+    return str3
