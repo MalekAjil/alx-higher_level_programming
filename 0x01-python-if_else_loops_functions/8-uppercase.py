ca4c9a78548ec4034ae5f890ba5433ab949b5036
MalekAjil <aboanasajil@gmail.com>
2023-12-26 11:13:18 -0800

    task 7

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         d = ord(c)
         if d > 96 and d < 123:
             d -= 32
-        print("{:c}".format(d), end = '')
+        print("{:c}".format(d), end='')
     print()
