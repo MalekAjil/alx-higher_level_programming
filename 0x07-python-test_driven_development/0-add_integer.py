2e6a4ea5229eb9413e302ddc845c7a60550c3bf7
MalekAjil <aboanasajil@gmail.com>
2024-01-03 11:41:21 -0800

    Add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
 def add_integer(a, b=98):
+    """ Returns the sum of two integers"""
     try:
         if (type(a) != float and type(a) != int):
             raise TypeError("a must be an integer")
