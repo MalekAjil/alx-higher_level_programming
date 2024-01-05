894617514c2f52cf99eb0af01678663123192fa6
MalekAjil <aboanasajil@gmail.com>
2024-01-05 12:30:57 -0800

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+""" Rectangle Class """
+
+
+class Rectangle:
+    """ Represents a rectangle
+    Arg __width: the width of the rectangle
+    Arg __heigh: the height of the rectangle
+
+    area: returns the area of the rectangle
+    perimeter: returns perimeter
+    """
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def print():
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        r = ""
+        for h in range(self.__height):
+            for w in range(self.__width):
+                r += "#"
+            r += "\n"
+        return r
+
+    def str():
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        r = ""
+        for h in range(self.__height):
+            for w in range(self.__width):
+                r += "#"
+            r += "\n"
+        return r
