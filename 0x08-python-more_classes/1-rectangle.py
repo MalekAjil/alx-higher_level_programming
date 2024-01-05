674b67559f0eeaf8587a411e4bae3a9e039b1345
MalekAjil <aboanasajil@gmail.com>
2024-01-04 19:06:18 -0800

    task 0

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,11 +7,16 @@ class Rectangle:
     Arg __width: the width of the rectangle
     Ard __heigh: the height of the rectangle
     """
-    #__width
-    #__height
-
     def __init__(self, width=0, height=0):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     def width(self):
