657dd7b1d6e77614c9aae1fb216cc05adadfed7a
MalekAjil <aboanasajil@gmail.com>
2024-01-08 12:13:59 -0800

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module MyList
+"""
+
+
+class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        prints the list, but sorted (ascending sort)
+
+        Args:
+            self: the object
+        """
+        new = self[:]
+        t = 0
+        for i in range(len(new)):
+            for j in range(i + 1, len(new)):
+                if new[i] > new[j]:
+                    t = new[i]
+                    new[i] = new[j]
+                    new[j] = t
+        print(new)
