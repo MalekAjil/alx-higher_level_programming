de910f1200abec7a1a8b3b889ca3690184c71243
MalekAjil <aboanasajil@gmail.com>
2024-01-07 19:31:32 -0800

    task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Print Square module
+"""
+
+
+def print_square(size):
+    """ prints a square
+
+    Args:
+        size: the size legth of the square
+
+    Raises:
+        TypeError if size is not an integer
+    """
+    try:
+
+        if type(size) is not int or (type(size) is float and size < 0):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        for i in range(size):
+            for j in range(size):
+                print("#", end='')
+            if i != size - 1:
+                print()
+    except (TypeError, ValueError) as tv:
+        print(tv)
