f5a842f856764e70e8fed9975e7207426e97d9a7
MalekAjil <aboanasajil@gmail.com>
2024-01-09 11:15:30 -0800

    task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module Write File
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a string to a text file (UTF8) and returns the number of characters written
+
+    Args:
+        filename: the file name
+        text: the text to be written
+    """
+    if filename is None or filename == "":
+        return
+    with open(filename, 'w', encoding="UTF8") as fn:
+        return fn.write(text)
