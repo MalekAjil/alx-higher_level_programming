3011d3a86adc065a92b80b52f2c89d35d9ca1620
MalekAjil <aboanasajil@gmail.com>
2024-01-08 18:35:37 -0800

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def area(self):
+        """
+        returns area
+        """
+        raise Exception("area() is not implemented")
