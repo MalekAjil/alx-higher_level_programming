d34714a8dc387984a5e064b48cc2a56a0ca46fda
MalekAjil <aboanasajil@gmail.com>
2024-01-08 19:34:12 -0800

    task13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module MyAttribute
+"""
+
+
+def add_attribute(self, name, value):
+    """
+    adds a new attribute to an object if it’s possible:
+
+    Raise a TypeError exception, with the message 
+    can't add new attribute if the object can’t have new attribute     
+    """
+    pass
