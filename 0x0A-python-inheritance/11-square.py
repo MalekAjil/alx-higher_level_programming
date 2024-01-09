80731419b85f0f1e07e64468d380df9f790313a6
MalekAjil <aboanasajil@gmail.com>
2024-01-08 19:22:57 -0800

    task11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a Square
+    """
+    def __init__(self, size):
+        """
+        intializes an object
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """
+        returns the area of rectangle
+        """
+        return self.__size * self.__size
+
+    def __print__(self):
+        """
+        prints the Square
+        """
+        print(f"[Square] {self.__size}/{self.__size}")
+
+    def __str__(self):
+        """
+        returns the Square string
+        """
+        return (f"[Square] {self.__size}/{self.__size}")
