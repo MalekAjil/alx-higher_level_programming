68c3958da0bc5eaa02569119a9f5bd07a36f0f2a
MalekAjil <aboanasajil@gmail.com>
2024-01-16 13:17:00 -0800

    task20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """
 import json
 import os
+import csv
 
 
 class Base:
@@ -78,4 +79,44 @@ class Base:
             str = fp.read()
         list_dicts = cls.from_json_string(str)
         list_instances = [cls.create(**d) for d in list_dicts]
+        return list_instances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serialises in CSV
+        Args:
+            cls: the class
+            list_objs: list of objects
+        """
+        fn = cls.__name__+".csv"
+        if list_objs is None:
+            list_objs = []
+        with open(fn, "w", newline="") as f:
+            writer = csv.writer(f)
+            for obj in list_objs:
+                dict_obj = obj.to_dictionary()
+                if cls.__name__ == "Rectangle":
+                    list_obj = [dict_obj["id"], dict_obj["width"],
+                            dict_obj["height"], dict_obj["x"], dict_obj["y"]]
+                elif cls.__name__ == "Square":
+                    list_obj = [dict_obj["id"], dict_obj["size"],
+                            dict_obj["x"], dict_obj["y"]]
+                writer.writerow(list_obj)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes in CSV"""
+        fn = cls.__name__+".csv"
+        list_instances = []
+        with open(fn, "r", newline="") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if cls.__name__ == "Rectangle":
+                    dict_obj = {"id": int(row[0]), "width": int(row[1]),
+                            "height": int(row[2]), "x": int(row[3]), "y": int(row[4])}
+                elif cls.__name__ == "Square":
+                    dict_obj = {"id": int(row[0]), "size": int(row[1]),
+                            "x": int(row[2]), "y": int(row[3])}
+                instance = cls.create(**dict_obj)
+                list_instances.append(instance)
         return list_instances
