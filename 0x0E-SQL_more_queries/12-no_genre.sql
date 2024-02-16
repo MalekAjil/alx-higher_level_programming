aefa2b9f5d0953ae0111e559d1bb977f0074ac02
MalekAjil <aboanasajil@gmail.com>
2024-02-15 19:09:03 -0800

    task12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT OUTER JOIN tv_show_genres
-ON tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title
