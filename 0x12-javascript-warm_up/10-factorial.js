6ffacc76c98af2d3f6b007da9294fa9ed56613bd
MalekAjil <aboanasajil@gmail.com>
2024-03-12 12:37:15 -0700

    task10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const n = parseInt(process.argv[2]);
+function factorial(x) {
+  if (x <= 1) {
+    return (1);
+  } else {
+    return (x * factorial(x - 1));
+  }
+}
+if (isNaN(n)) {
+  console.log(1);
+} else {
+  console.log(factorial(n));
+}
