3bbf7b798a382646d5dc242d3bc9f41d71a66bdd
MalekAjil <aboanasajil@gmail.com>
2024-03-12 13:02:34 -0700

    task11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  let max = 0;
+  let smax = 0;
+  for (let i = 2; i < process.argv.length; i++) {
+    let x = parseInt(process.argv[i]);
+    if (x > smax) { smax = x;}
+    if (smax > max) {
+      let t = max;
+      max = smax;
+      smax = t;
+    }
+  }
+  console.log(smax);
+}
