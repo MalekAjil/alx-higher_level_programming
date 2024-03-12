3ce8188e5bb756cc716a7ab87b8589bc0cda280f
MalekAjil <aboanasajil@gmail.com>
2024-03-12 12:22:39 -0700

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,14 @@ const n = parseInt(process.argv[2]);
 if (isNaN(n)) {
   console.log('Missing size');
 } else {
+  let s = '';
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      console.log('X');
+      s += 'X';
     }
-    console.log();
+    if (i < n - 1) {
+      s += '\n';
+    }
   }
+  console.log(s);
 }
