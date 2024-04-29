85471724ad96d7e4168797b8a14ae3d9362d9e72
MalekAjil <aboanasajil@gmail.com>
2024-04-29 04:32:07 -0700

    task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,8 +14,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=database)
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states \
-                WHERE name = '{name}' COLLATE utf8mb4_bin ORDER BY id")
+    cur.execute("SELECT * FROM states \
+                WHERE name = '{}' COLLATE utf8mb4_bin \
+                ORDER BY id".format(name))
     rows = cur.fetchall()
     for row in rows:
         print(row)
