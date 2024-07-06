3b31bb0ecc72611333db5f98f9f9c5112dc65598
MalekAjil <aboanasajil@gmail.com>
2024-07-06 13:30:44 -0700

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """HiddenNames"""
-import sys
 import hidden_4
 
+
 if __name__ == "__main__":
-    names = hidden_4
-    print(names)
+    names = dir(hidden_4)
+    for n in names:
+        if n[0] != '_':
+            print(n)
