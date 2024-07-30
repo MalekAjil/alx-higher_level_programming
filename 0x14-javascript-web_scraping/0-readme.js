567aada9ec0404f766efc5d7de7b6394162d935f
MalekAjil <aboanasajil@gmail.com>
2024-07-30 16:23:54 -0700

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
+const fs = require('fs');
 
+function printFile(fPath) {
+  fs.readFile(fPath, 'utf-8', (err, txt) => {
+    if (err) {
+      console.error('Error: ${err.message}');
+    } else {
+      console.log(txt);
+    }
+  });
+}
+
+const fPath = process.argv[2];
+printFile(fPath);
