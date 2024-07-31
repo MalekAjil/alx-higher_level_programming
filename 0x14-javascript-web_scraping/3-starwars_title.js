d78b3a023f53e542671617f9a8f0eaf892808f50
MalekAjil <aboanasajil@gmail.com>
2024-07-30 17:10:15 -0700

    task3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 
-function tarwarsTitle(id) {
+function starwarsTitle(id) {
   request(`https://swapi-api.alx-tools.com/api/films/${id}`, (err, res) => {
     if (err) {
       console.error(`Error: ${err.message}`);
     } else {
-      console.log(`code: ${res.title}`);
+      console.log(`${res.title}`);
     }
   });
 }
