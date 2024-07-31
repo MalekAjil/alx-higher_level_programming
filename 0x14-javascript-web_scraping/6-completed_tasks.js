1ba86c7cf2d39af6122c99d088232fc3b3729566
MalekAjil <aboanasajil@gmail.com>
2024-07-30 18:51:54 -0700

    task6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,11 +7,11 @@ function starwarsCount (url) {
       console.error(`Error: ${err.message}`);
     } else if (res.statusCode === 200) {
       const tasksList = JSON.parse(content);
-      const userCompleted= {};
+      const userCompleted = {};
       tasksList.forEach((task) => {
-	if (task.completed) {
+        if (task.completed) {
           const uId = task.userId;
-	  userCompleted[uId] = (userCompleted[uId] || 0) + 1;
+          userCompleted[uId] = (userCompleted[uId] || 0) + 1;
         }
       });
       console.log(userCompleted);
