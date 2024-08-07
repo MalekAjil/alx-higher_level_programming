0a8cdb871d8fa1efabd2f6cf92b322d17fd504d9
MalekAjil <aboanasajil@gmail.com>
2024-08-07 16:07:58 -0700

    task2

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("DIV #red_header").click(function() {
-    $("DIV #red_header").css("color", "#FF0000");
+  $("#red_header").click(function() {
+    $("header").css("color", "#FF0000");
   });
 });
