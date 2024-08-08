66b36da9ba1de935453905fe3b391263b57c07c4
MalekAjil <aboanasajil@gmail.com>
2024-08-07 17:33:14 -0700

    task8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,10 +1,10 @@
 $(function () {
   const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
   $.get(url, function(data, textStatus) {
-    for (let film in data.results) {
+    data.results.forEach(function(film) {
       const Item = $('<li></li>').text(film.title);
       $('UL#list_movies').append(Item);
 	    alert(film);
-    }
+    });
   });
 });
